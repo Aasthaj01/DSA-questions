ebdd7ff6486656975c468c50d3c4264d3cad6c0a
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-14 10:49:42 +0530

    Creating a stack
    Creating a stack by taking inputs from user

---
+++ stack/initial_stack.py
@@ -0,0 +1,33 @@
+# Stack problems
+# ------Reversing a stack ------ 
+class stack():
+    def __init__(self):
+        self.items = []
+        
+    def push(self, item):
+        self.items.append(item)
+    
+    def is_empty(self):
+        if self.items == []:
+            return
+    def pop(self):
+        return self.items.pop()
+        
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+    
+    def display(self):
+        for item in reversed(self.items):
+            print(item)        
+            
+            
+s = stack()  
+num = int(input("How many numbers you want in the stack?"))
+entries = list(map(int, input("Enter the numbers: ").split()))
+# print(entries)
+for i in range(0, num):
+    s.push(entries[i])
+ 
+print('The stack is:')
+s.display()
