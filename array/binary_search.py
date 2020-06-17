03b162d4d86497530b6b47841ecf1d3c5231083c
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-17 11:22:14 +0530

    Create binary_search.py

---
+++ array/binary_search.py
@@ -0,0 +1,25 @@
+# finds element in array by using binary search
+
+def binarySearch(arr, low, high, key): 
+  
+    if (high < low): 
+        return -1
+    
+    mid = (low + high)/2
+  
+    if (key == arr[int(mid)]): 
+        return mid 
+  
+    if (key > arr[int(mid)]): 
+        return binarySearch(arr, 
+           (mid + 1), high, key) 
+  
+    return (binarySearch(arr, low, 
+           (mid -1), key)) 
+  
+
+arr = list(map(int, input("Enter the nmbers in the array:").split()))
+n = len(arr) 
+key = int(input("Enter the number you want to search:"))
+print("Index:", int(binarySearch(arr, 0, n, key) )) 
+  
