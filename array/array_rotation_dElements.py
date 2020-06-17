753de7889ca78bdd2c2d1768d4f92de2459a0c0b
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-17 13:23:29 +0530

    Create array_rotation_dElements.py

---
+++ array/array_rotation_dElements.py
@@ -0,0 +1,35 @@
+# Rotating the array by d elements clockwise
+
+def print_array(arr, n):
+    for i in range(n):
+        print(arr[i], end = " ")
+# rotation of array of length n by d elements
+
+
+def array_rotation(arr, n, d):
+    for i in range(0, d):
+        temp = arr[0]
+        for j in range(0, n-1): 
+          arr[j] = arr[j+1] 
+        arr[n-1] = temp 
+    print_array(arr, n)    
+    
+    
+no_of_testCases = int(input("Enter the no of test cases you want:"))
+for i in range(no_of_testCases):
+    listt = list(map(int, input("Enter size of array and size of rotation:").split()))
+    n = listt[0]
+    d = listt[1]
+    
+    arr = list(map(int, input("Enter array elements:").split()))
+    print("array before rotation is")
+    for i in range(n):
+        print(arr[i], end = " ")
+    print()
+    print("The array after rotation is:")
+    array_rotation(arr, n, d)
+    print()
+    
+    
+     
+
