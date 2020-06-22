8392f387bcb4f387a8abf6251ed21591e951ef3a
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-22 11:10:04 +0530

    Create insert_beginning.py

---
+++ LinkedList/insert_beginning.py
@@ -0,0 +1,40 @@
+# creating basic llist and appending node at beginning of llist
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next =None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        
+    def print_list(self):
+        cur_node = self.head 
+        while cur_node:
+            print(cur_node.data, end = " --> ")
+            cur_node = cur_node.next
+            
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        
+    def append(self, data):
+        new_node = Node(data)
+        
+        if self.head is None:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node= last_node.next
+        last_node.next = new_node
+        
+llist = LinkedList()
+arr = list(map(int, input("Enter the number you waant in your linked list:").split()))
+for i in range(0, len(arr)):
+    llist.append(arr[i])
+llist.prepend("45")
+llist.print_list()    
+            
