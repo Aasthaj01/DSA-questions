c7e69bb072a42365cb641266fb38af7e3023f569
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-24 11:06:31 +0530

    Update fibonacci.py

--- Dyanamic Programming/fibonacci.py
+++ Dyanamic Programming/fibonacci.py
@@ -1,13 +1,29 @@
 
-#Recursion
+#Recursion--------------------------O(2^n)
 def fib(num):
     if num <= 1:
       return num
     else:    
         return (fib(num-1) + fib(num-2))
-        
-        
-# Top Down approach - fibonacci        
+  
+#--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Bottom Down approach--------------O(n)  
+def fibonacci_bottom_up(num):
+  arr[0] = 0
+  arr[1] = 1
+
+  for i in range(2, num+1):
+    arr[i] = arr[i-1] + arr[i-2]
+  return arr[num]
+
+
+num = int(input("Enter the number till which you want to find fib sequence:"))
+arr = [-1]*50
+for i in range(0, num+1):
+    print(fibonacci_bottom_up(i))
+
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Top Down approach----------------O(2n-1) i.e O(n)     
 def fib_topDown(num, arr):
     if num <= 1:
         arr[num] = num
