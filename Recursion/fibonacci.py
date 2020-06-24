250fb00b9c2d166c2edbf217e7cc8ba31105d822
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-24 10:14:50 +0530

    Create fibonacci.py

---
+++ Recursion/fibonacci.py
@@ -0,0 +1,13 @@
+# Recursion - fabonacci
+
+def fib(num):
+    if num <= 1:
+       return num
+    else:    
+        return (fib(num-1) + fib(num-2))
+    
+    
+num = int(input("Enter the number till which you want to find fib:"))
+
+for i in range(num):
+       print(fib(i))
