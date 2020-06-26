b22dc84356fcf7e246e0a5e4a8a78623128e68d5
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-06-26 12:06:16 +0530

    Update min_coin_change.py

--- Dyanamic Programming/min_coin_change.py
+++ Dyanamic Programming/min_coin_change.py
@@ -1,5 +1,5 @@
 #Given a amount, if we want to make change for amount, and we have infinite supply of each of coins = { C1, C2, .. , Cm}, what is the minimum number of coins to make the change?
-
+#---------------------------RECURSION---------------------------------------------------------------------------------------------------------------
 import sys
 
 def coins_needed(coins, amount, n):
@@ -20,3 +20,42 @@ coins = list(map(int, input("Enter the coins available:").split()))
 n=  len(coins)
 amount = int(input("Enter the amount:"))
 print(coins_needed(coins, amount, n))
+
+#------------------------------------DYNAMIC PROGRAMMING------------------------------------------------------------------
+
+import sys
+
+def coins_needed(coins, n, amount): 
+      
+    # table[i] will be storing the minimum  
+    # number of coins required for i value.  
+    # So table[amount] will have result 
+    table = [0 for i in range(amount + 1)] 
+  
+    # Base case (If given value amount is 0) 
+    table[0] = 0
+  
+    # Initialize all table values as Infinite 
+    for i in range(1, amount + 1): 
+        table[i] = sys.maxsize 
+  
+    # Compute minimum coins required  
+    # for all values from 1 to amount 
+    for i in range(1, amount + 1): 
+          
+        # Go through all coins smaller than i 
+        for j in range(n): 
+            if (coins[j] <= i): 
+                sub_res = table[i - coins[j]] 
+                if (sub_res != sys.maxsize and 
+                    sub_res + 1 < table[i]): 
+                    table[i] = sub_res + 1
+    return table[amount] 
+  
+
+if __name__ == "__main__": 
+    coins = list(map(int, input().split()))    
+    n=  len(coins)
+    amount = int(input())
+    print(coins_needed(coins, n, amount))
+     
