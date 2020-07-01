84a0b0a96f2dd7dce67274b07c15cae94babcc1a
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-01 15:56:04 +0530

    Added bottom up approach

--- Dyanamic Programming/LCS.py
+++ Dyanamic Programming/LCS.py
@@ -29,5 +29,36 @@ memo = [[-1 for i in range(m+1)]for j in range(n+1)]
 
 
 print(longest_sub(s1, s2, m, n))
+#----------------------------------------------------------------------------------------------------------------------------------------------------------
+# Bottom up approach or tabulation method
+
+def lcs(s1, s2, m, n):
+    
+    dp = [[-1]*(n+1)]*(m+1)
+    
+    for i in range(0, m+1):
+        dp[i][0] = 0
+        for j in range(0, n+1):
+            dp[0][j] = 0
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            
+           
+            if s1[i-1]==s2[j-1]:
+                dp[i][j] = 1+dp[i-1][j-1]
+                # return 1+lcs(s1, s2, m-1, n-1)
+            else:
+                
+                dp[i][j] =  max(dp[i][j-1], dp[i-1][j])
+    return dp[m][n]    
+        
+s1 = str(input())
+m = len(s1)
+s2 = str(input())
+n = len(s2)
+print(lcs(s1, s2, m, n))
+
+
+
 
 
