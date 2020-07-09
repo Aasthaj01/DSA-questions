fd022813030d42182a2fb430c2633c4a2956fdd9
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-09 12:52:03 +0530

    Update reverse_bfs.py

--- Tree/reverse_bfs.py
+++ Tree/reverse_bfs.py
@@ -98,5 +98,77 @@ b.root.right = Node(3)
 b.root.left.left = Node(10)
 b.root.right.left = Node(20)
 print(b.print_tree("reverse order"))
+#------------------------------------------------------------------------------------------------------------------------
+#METHOD 2 - Using library
+
+# reverse bfs traversal
+from collections import deque
+from queue import Queue
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+        
+class BinaryTree(object):
+    def __init__(self, root):
+        self.root = Node(root)
+    def print_tree(self, traversal_type):
+        if traversal_type == "reverse levelorder" or traversal_type == "reverse order" or traversal_type == "reverse bst":
+            return self.reverse_level_order(b.root)    
+        else:
+            print("traversal type"+ "- " +str(traversal_type)+" "+"is not supported")
+            return False
+    def level_order(self, start):
+        if start is None:
+            return 
+        q =Queue()
+        q =Queue(maxsize = 30)
+        
+        q.put(start)
+        traversal = ""
+        while q.qsize()>0:
+            traversal+= str(q.first())+ "-->"
+            node = q.get()
+            if node.left:
+                q.put(node.left)
+            if node.right:
+               
+                q.put(node.right)
+        return traversal            
+    def reverse_level_order(self, start):
+        if start is None:
+            return
+        s = deque()
+        q = Queue(maxsize = 30)
+      
+        q.put(start)
+        traversal = ""
+        
+        while q.qsize()>0:
+            
+            node = q.get()
+            s.append(node)
+            if node.right:
+                q.put(node.right)
+            if node.left:
+                q.put(node.left)
+                
+        while len(s)>0:
+            node = s.pop()
+            traversal += str(node.value)+"-"
+        return traversal
+
+            
+b = BinaryTree(1)        
+b.root.left = Node(2)
+b.root.right = Node(3)
+b.root.left.left = Node(10)
+b.root.right.left = Node(20)
+print(b.print_tree("reverse order"))
+    
+
+
     
 
