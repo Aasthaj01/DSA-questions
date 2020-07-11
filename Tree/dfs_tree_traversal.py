c354c404aac2344ab00e00485ac7dd59c645760e
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-11 17:38:42 +0530

    Update dfs_tree_traversal.py

--- Tree/dfs_tree_traversal.py
+++ Tree/dfs_tree_traversal.py
@@ -49,3 +49,79 @@ b.root.right.left = Node(20)
 print(b.print_tree("Preorder"))
 print(b.print_tree("Inorder"))
 print(b.print_tree("postorder"))
+#-------------------------------------------------------------------------------------------------------------------------
+# Finding max width. Maximum Width of Binary tree is the maximum number of nodes present in a level of the Tree.
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+
+    
+def pre_order(node):
+    if node is None:
+        return 
+    # root, left, right
+    print(node.value, end = " ")
+    pre_order(node.left)
+    pre_order(node.right)
+    
+def post_order(node):
+    if node is None:
+        return 
+    # left, right, root
+    pre_order(node.left)
+    pre_order(node.right) 
+    print(node.value, end = " ")
+    
+def in_order(node):
+    if node is None:
+        return 
+    # left, root, right
+    pre_order(node.left)
+    print(node.value, end = " ")
+    pre_order(node.right) 
+    
+def level_order(node):
+    if node is None: 
+        return
+    queue = [] 
+    queue.append(node) 
+    while(len(queue) > 0): 
+        print (queue[0].value) 
+        node = queue.pop(0)
+        if node.left is not None: 
+            queue.append(node.left)
+        if node.right is not None: 
+            queue.append(node.right)
+    
+# def level(arr, node, i, n):
+#     if i <n:
+#         temp = Node(arr[i])
+#         node = temp
+         
+#         node.left = level(arr, node.left, 2*i+1, n)
+#         node.right = level(arr, node.right, 2*i+2, n)
+#     return root    
+        
+# arr = list(map(int, input().split())) 
+# n = len(arr)
+# root = None
+# root = level(arr, root, 0, n)
+
+root = Node(1)
+root.left = Node(2)
+root.right = Node(5)
+root.left.left = Node(2)
+root.left.right =Node(5)
+root.left.left.left = Node(2)
+print("preorder traversal of tree is:") 
+pre_order(root)
+print("\npost order traversal of tree is:")
+post_order(root)
+print("\ninorder traversal of tree is:")
+in_order(root)
+
+
+
