e384aa6bdc5d21686d69f8394820800b382907ec
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-13 22:58:57 +0530

    Update constuct_tree_from_pre_inorder.py

--- Tree/constuct_tree_from_pre_inorder.py
+++ Tree/constuct_tree_from_pre_inorder.py
@@ -39,4 +39,33 @@ buildTree.preIndex = 0
 root = buildTree(inOrder, preOrder, 0, len(inOrder)-1) 
 print("Postorder traversal of the constructed tree is:")
 printPostorder(root) 
+#------------------------------------------------------------------------------------------------------------------------------------
+
+class TreeNode:
+   def __init__(self, data, left = None, right = None):
+      self.data = data
+      self.left = left
+      self.right = right
+      
+def print_tree(root):
+   
+   if root is not None:
+      print_tree(root.left)
+      print_tree(root.right)
+      print(root.data, end = ' ')
+      
+class Solution(object):
+   def buildTree(self, preorder, inorder):
+       if inorder:
+          root = TreeNode(preorder.pop(0))
+          root_index = inorder.index(root.data)
+          root.left = self.buildTree(preorder,inorder[:root_index])
+          root.right = self.buildTree(preorder,inorder[root_index+1:])
+          return root
+      
+ob1 = Solution()
+preorder = list(map(int, input("Enter preorder traversal:").split()))
+inorder = list(map(int, input("Enter inorder traversal:").split()))
+print("The postorder traversal of the tree is:")
+print_tree(ob1.buildTree(preorder, inorder))
    
