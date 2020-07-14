c55988d43420a99ee2245aebc99bcbbbe84fea2f
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-14 13:38:35 +0530

    Update spiral_level_order.py

--- Tree/spiral_level_order.py
+++ Tree/spiral_level_order.py
@@ -11,61 +11,86 @@ class Node(object):
         self.left = None
         self.right = None
         
-def spiral_level(node):
-    
-    if node == 0:
+def spiral_traversal_left(node):
+    if node is None:
         return
-    
     s1 = []
     s2 = []
     s1.append(node)
-    while not len(s1) == 0 or not len(s2) == 0:
-        
+    while not len(s1) == 0 or  not len(s2) == 0:
         while not len(s1) == 0:
             temp = s1[-1]
-            s1.pop()  
-            print(temp.value, end = " ")  
-   
-            if (temp.right):  
-                s2.append(temp.right)  
-            if (temp.left): 
-                s2.append(temp.left) 
+            s1.pop()
+            print(temp.value, end = " ")
+            
+            if temp.right:
+                s2.append(temp.right)
+            if temp.left:    
+                s2.append(temp.left)
+                
         while not len(s2) == 0:
-            temp = s2[-1]  
-            s2.pop()  
-            print(temp.value, end = " ")  
-            if (temp.left): 
-                s1.append(temp.left) 
-            if (temp.right):  
+            temp = s2[-1]
+            s2.pop()
+            print(temp.value, end = " ")
+            
+            if temp.left:    
+                s1.append(temp.left)
+            if temp.right:
                 s1.append(temp.right)
                 
-  
+def spiral_traversal_right(node):
+    if node is None:
+        return
+    s1 = []
+    s2 = []
+    s1.append(node)
+    while not len(s1) == 0 or  not len(s2) == 0:
+        while not len(s1) == 0:
+            temp = s1[-1]
+            s1.pop()
+            print(temp.value, end = " ")
+            if temp.left:    
+                s2.append(temp.left)
+            if temp.right:
+                s2.append(temp.right)
             
-# def level_order(arr, node, i, n):    
-#     if i<n:
-#         temp = Node(arr[i])
-#         node = temp
-#         node.left = level_order(arr, node.left, 
-#                                      2 * i + 1, n)  
-
-#         node.right = level_order(arr, node.right, 
-#                                       2 * i + 2, n) 
-#     return node 
-  
-# arr = list(map(int, input().split()))   
-# n = len(arr)
-# root = None
-# root = level_order(arr, root, 0, n)
-# print(root)            
+                
+        while not len(s2) == 0:
+            temp = s2[-1]
+            s2.pop()
+            print(temp.value, end = " ")
+            if temp.right:
+                s1.append(temp.right)
+            if temp.left:    
+                s1.append(temp.left)
+    
+def construct_tree(arr, node, i, n):
+    if i<n:
+        temp = Node(arr[i])
+        node = temp
+        node.left = construct_tree(arr, node.left, 2*i+1, n)
+        node.right = construct_tree(arr, node.right, 2*i+2, n)
+    return node  
+    
+    
+arr = list(map(int, input("Enter the nodes:").split()))
+n = len(arr)
+root = None
+root = construct_tree(arr, root, 0, n)
+print("The left spiral traversal of the tree is:")
+spiral_traversal_left(root)
+print("\nThe right spiral traversal of the tree is:")
+spiral_traversal_right(root)
+            
             
 
-root = Node(1)  
-root.left = Node(2)  
-root.right = Node(3)  
-root.left.left = Node(7)  
-root.left.right = Node(6)  
-root.right.left = Node(5)  
-root.right.right = Node(4)  
-print("Spiral Order traversal of binary tree is: ")
-spiral_level(root)                    
+# root = Node(1)  
+# root.left = Node(2)  
+# root.right = Node(3)  
+# root.left.left = Node(7)  
+# root.left.right = Node(6)  
+# root.right.left = Node(5)  
+# root.right.right = Node(4)  
+# print("Spiral Order traversal of binary tree is: ")
+# spiral_level(root)                    
 
