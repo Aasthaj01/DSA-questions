6a196d74ead7041c3a854662d0f105d48f61837f
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-15 18:47:58 +0530

    Update tree_size.py

--- Tree/tree_size.py
+++ Tree/tree_size.py
@@ -46,3 +46,39 @@ b.root.right.left = Node(30)
 
 print(b.tree_size(b.root))
 print(b.tsize())
+#-------------------------------------------------------------------------------------------------------
+class TreeNode:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+class Solution:
+    def countNodes(self, root: TreeNode) -> int:
+        if not root: return 0
+
+        def dfs(node):
+            if not node: return 0
+
+            left = node.left
+            left_count = 1
+            while left:
+                left = left.left
+                left_count += 1
+
+            right = node.right
+            right_count = 1
+            while right:
+                right = right.right
+                right_count += 1
+
+            if left_count == right_count:
+                return (2**left_count) - 1
+            else:
+                left = dfs(node.left)
+                right = dfs(node.right)
+
+                return left + right + 1
+        
+        return dfs(root)
+    
+
