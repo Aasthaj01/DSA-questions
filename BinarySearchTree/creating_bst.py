314c3eef4195e585aa97f632d89b07daf09892f1
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-16 19:34:00 +0530

    Update creating_bst.py

--- BinarySearchTree/creating_bst.py
+++ BinarySearchTree/creating_bst.py
@@ -1,4 +1,4 @@
-#  create a binary search tree from given data and searching a particular input node
+#  create a binary search tree from given data
 
 class Node(object):
     def __init__(self, value):
@@ -6,16 +6,16 @@ class Node(object):
         self.right = None
         self.left = None
         
-def bst_search(node, num):
-    if node is None:
-        return False
+# def bst_search(node, num):
+#     if node is None:
+#         return False
 
-    if num == node.value:
-        return True
-    if num <node.value:
-        return bst_search(node.left, num)
-    else:    
-        return bst_search(node.right, num) 
+#     if num == node.value:
+#         return True
+#     if num <node.value:
+#         return bst_search(node.left, num)
+#     else:    
+#         return bst_search(node.right, num) 
         
 def insert(root, node):
      
@@ -60,8 +60,51 @@ for i in range(0, n):
     insert(r, Node(arr[i]))
 print(inorder(r))   
 
-num = int(input("Enter the number to be searched:"))
-print(bst_search(r, num))
+# num = int(input("Enter the number to be searched:"))
+# print(bst_search(r, num))
+#=============================================================================================================================
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+        
+def insert(node, key):
+    
+    if node is None: 
+        return Node(key)
+    if key < node.value: 
+        node.left = insert(node.left, key) 
+    else: 
+        node.right = insert(node.right, key) 
+    return node
+
+def inorder(root): 
+    if root: 
+        inorder(root.left) 
+        print(root.value, end = " ") 
+        inorder(root.right)
+        
+def insertLevelOrder(arr, root, i, n): 
+    if i < n: 
+        temp = Node(arr[i])  
+        root = temp  
+  
+        root.left = insertLevelOrder(arr, root.left, 
+                                     2 * i + 1, n)  
+
+        root.right = insertLevelOrder(arr, root.right, 
+                                      2 * i + 2, n) 
+    return root 
+
+
+arr = list(map(int, input("Enter nodes other than root node:").split()))
+n = len(arr) 
+a= int(input("Enter root node:"))
+r = Node(a)
+for i in range(0, n):
+    insert(r, arr[i])
+print(inorder(r))   
 
 #=============================================================================================================================
 #Iterative approach where space complexity is o(1)
