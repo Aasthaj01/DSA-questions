6fbf2fc9b9a6c4e7c513d7a6c610fdab6f9cefab
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-21 22:57:38 +0530

    Update ceil_left_side.py

--- array/ceil_left_side.py
+++ array/ceil_left_side.py
@@ -16,7 +16,21 @@ def ceil_left_side(arr, n):
         else:
             print(arr[i]+diff, end = " ")
 
+def ceil_nlogn(arr, n):
+  
+    s = set() 
+    for i in range(0, n):  
+        it = [x for x in s if x >= arr[i]]  
+        if len(it) == 0:  
+            print("-1", end = " ")  
+        else:                  
+            print(min(it), end = " ")          
+        s.add(arr[i])             
+            
 arr = list(map(int, input("Enter the array elements: ").split()))
 n = len(arr)
+print("O(n**2) solution is:")
 ceil_left_side(arr, n)
+print("\n O(nlogn) solution is:")
+ceil_nlogn(arr, n)
     
