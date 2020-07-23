d0b8f4df10f74584f2b5e2d019358d60a527ff53
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-07-23 15:00:01 +0530

    Update creating_bst.py

--- BinarySearchTree/creating_bst.py
+++ BinarySearchTree/creating_bst.py
@@ -162,4 +162,51 @@ insert(root, 70)
 insert(root, 60)  
 insert(root, 80)  
 Inorder(root)
+#==================================================================================================================
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.right = None
+        self.left = None
+        
+def preOrder(root):
+    if root == None:
+        return
+    print (root.info, end=" ")
+    preOrder(root.left)
+    preOrder(root.right)
+    
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
 
+    def insert(self, val):
+        if self.root == None:
+            self.root = Node(val)
+
+        root = self.root
+        while True:
+            if val > root.info:
+                if root.right:
+                    root = root.right
+                else:
+                    root.right = Node(val)
+                    break
+            elif val < root.info:
+                if root.left:
+                    root = root.left
+                else:
+                    root.left = Node(val)
+                    break
+            else:
+                break
+
+tree = BinarySearchTree()
+t = int(input())
+
+arr = list(map(int, input().split()))
+
+for i in range(t):
+    tree.insert(arr[i])
+
+preOrder(tree.root)
