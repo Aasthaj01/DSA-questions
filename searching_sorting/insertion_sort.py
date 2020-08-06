b5884081f2e63a0506fb8436d665e70566ddbae3
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-08-07 00:01:10 +0530

    Create insertion_sort.py

---
+++ searching_sorting/insertion_sort.py
@@ -0,0 +1,25 @@
+# recursive insertion sort
+
+def insertionSort(arr,n):
+   # base case
+   if n<=1:
+      return
+   # Sort
+   insertionSort(arr,n-1)
+   last = arr[n-1]
+   j = n-2
+  
+   while (j>=0 and arr[j]>last):
+      arr[j+1] = arr[j]
+      j = j-1
+   arr[j+1]=last
+   
+   
+   
+n = int(input("How many elements you want in the array?"))
+arr = list(map(int, input("Enter the elements:").split()))
+insertionSort(arr, n)
+print("Sorted array is:")
+for i in range(n):
+   print(arr[i],end=" ")        
+            
