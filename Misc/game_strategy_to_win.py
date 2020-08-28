afbd70b271616595f20fe09e80ffd6b083f4b729
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-08-28 16:21:02 +0530

    Create game_strategy_to_win.py

---
+++ Misc/game_strategy_to_win.py
@@ -0,0 +1,19 @@
+# optimal strategy for is game is when you pick first.
+# you can either choose 1st or last element and then second player get to choose from remaining array.
+# You need to win the game
+
+def optimal_strategy(arr, n):
+    sume, sumo = 0, 0
+    for i in range(1, n, 2):
+        sume+= arr[i]
+    for i in range(0, n, 2):
+        sumo+= arr[i] 
+    if sume>sumo:
+        return "pick even"
+    if sume<sumo:
+        return "pick odd"           
+    # return max(sume, sumo)    
+
+arr = list(map(int, input().split()))
+n = len(arr)
+print(optimal_strategy(arr, n))
