02f2168c4afc8efce09ece8c887f6c03e07dc0c6
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-08-30 14:00:32 +0530

    Update is_sorted.py

--- array/is_sorted.py
+++ array/is_sorted.py
@@ -1,20 +1,20 @@
 # The program will return true in both the cases i.e reversed sort or increasing sort
 
-def is_sorted(arr, n):
-    temp = [0]*n
-    for i in range(n):
-        temp[i] = arr[i]
-    temp.sort()
-    rev = temp
-    rev.sort(reverse=True)    
-    for i in range(n):
-        if arr[i] == temp[i]:
-            return True
-        elif arr[i] ==  rev[i]:
-            return True  
-        else:
-            return False 
-            
 arr = list(map(int, input().split()))    
 n = len(arr)
-print(is_sorted(arr, n))
+flag = 0
+temp = arr[:] 
+temp.sort()
+rev = arr
+rev.sort(reverse = True)
+if (temp == arr) or (rev == arr): 
+    flag = 1
+else:
+    flag = 0
+       
+       
+if (flag): 
+    print ("Yes, array is sorted")
+else:
+    print("Array is not sorted")
+
