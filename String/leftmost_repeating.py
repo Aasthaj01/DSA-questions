0572102dffee00347401f861f19747ec5777ae80
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-09-04 11:50:08 +0530

    added other method

--- String/leftmost_repeating.py
+++ String/leftmost_repeating.py
@@ -12,3 +12,37 @@ def check_left_repeating(string):
                 
 string = str(input())                
 print(check_left_repeating(string))
+
+#=======================================================
+# leftmost repeating character
+import sys
+
+def check_left_repeating(string):
+    arr = [0]*NO_OF_CHARS
+    for i in range(NO_OF_CHARS): 
+        arr[i] = -1
+  
+    res = sys.maxsize 
+    for i in range(len(string)): 
+        if (arr[ord(string[i])] == -1): 
+            arr[ord(string[i])] = i 
+        else: 
+            res = min(res, arr[ord(string[i])]) 
+  
+    if res == sys.maxsize: 
+        return -1
+    else: 
+        return res 
+  
+  
+NO_OF_CHARS = 256
+string = str(input())     
+index = check_left_repeating(string) 
+if (index == -1): 
+    print("Either all characters are distinct or string is empty") 
+else: 
+    print("First Repeating character is",string[index]) 
+                
+                
+               
+
