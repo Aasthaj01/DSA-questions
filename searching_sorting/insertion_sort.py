f6f9354cc31e71b180e8bc53edd2adde87d68d78
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-09-05 00:45:49 +0530

    added another method to implement the code

--- searching_sorting/insertion_sort.py
+++ searching_sorting/insertion_sort.py
@@ -21,5 +21,20 @@ arr = list(map(int, input("Enter the elements:").split()))
 insertionSort(arr, n)
 print("Sorted array is:")
 for i in range(n):
-   print(arr[i],end=" ")        
+   print(arr[i],end=" ")
+
+#==================================================================
+# Method 2
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        j = i-1
+        while j>=0 and arr[j]>arr[j+1]:
+            arr[j+1], arr[j] = arr[j], arr[j+1] 
+            j-=1
+            # break
+    return arr
+    
+arr = list(map(int, input().split()))
+print(insertion_sort(arr))
             
