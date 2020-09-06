251bf216c4fc42fd1b44c3cbf55cca80d291c042
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-09-06 16:15:11 +0530

    added recursive approach

--- LinkedList/reversing_llist.py
+++ LinkedList/reversing_llist.py
@@ -30,11 +30,23 @@ class LinkedList:
             prev =cur_node
             cur_node = nxt
         self.head = prev 
-
+    def recursive(self):
+        def reverse_recursive(cur_node, prev):
+            if not cur_node:
+                return prev
+            nxt = cur_node.next
+            cur_node.next = prev
+            prev = cur_node
+            cur_node = nxt
+            return reverse_recursive(cur_node, prev)
+        self.head = reverse_recursive(cur_node = self.head, prev = None)
 
 llist = LinkedList()
 arr = list(map(int, input("Enter the number you want in your linked list:").split()))
 for i in range(0, len(arr)):
     llist.append(arr[i])    
 llist.reverse_list()
+llist.print_list()
+# Method 2
+llist.recursive()
 llist.print_list()
