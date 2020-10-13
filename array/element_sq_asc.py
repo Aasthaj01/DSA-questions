44740968d94f4d486ada082ef18c5e266172abbb
Aasthaj01 <62432620+Aasthaj01@users.noreply.github.com>
2020-10-13 11:48:51 +0530

    added O(n) solution

--- array/element_sq_asc.py
+++ array/element_sq_asc.py
@@ -8,3 +8,30 @@ for i in range(n):
 del arr[:n]
 arr.sort()
 print(arr)
+#=================================================================================
+# O(n) solution
+def sortSquares(arr, n): 
+    left, right = 0, n - 1
+    index = n - 1
+    result = [0 for x in arr] 
+  
+    while index >= 0: 
+  
+        if abs(arr[left]) >= abs(arr[right]): 
+            result[index] = arr[left] * arr[left] 
+            left += 1
+        else: 
+            result[index] = arr[right] * arr[right] 
+            right -= 1
+        index -= 1
+      
+    for i in range(n):  
+        arr[i] = result[i]
+    return arr
+
+    
+arr = list(map(int, input().split()))
+n = len(arr)
+print(sortSquares(arr, n))
+
+
