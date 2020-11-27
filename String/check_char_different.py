3bcd877d83ca0e0b6d3fdceba478a7841a78401f
Aastha <62432620+Aasthaj01@users.noreply.github.com>
2020-11-27 23:43:41 +0530

    Create check_char_different.py

---
+++ String/check_char_different.py
@@ -0,0 +1,33 @@
+# Find and print the uncommon characters of the two given strings in sorted order.
+#Input: str1 = “characters”, str2 = “alphabets”
+#Output: b c l p r
+
+MAX_CHAR = 26
+
+def findAndPrintUncommonChars(str1, str2): 
+    present = [0] * MAX_CHAR 
+    for i in range(0, MAX_CHAR): 
+        present[i] = 0
+    l1 = len(str1) 
+    l2 = len(str2) 
+    
+    for i in range(0, l1): 
+        present[ord(str1[i]) - ord('a')] = 1 
+        
+    for i in range(0, l2): 
+    
+        if(present[ord(str2[i]) - ord('a')] == 1 or 
+           present[ord(str2[i]) - ord('a')] == -1): 
+            present[ord(str2[i]) - ord('a')] = -1
+        else: 
+            present[ord(str2[i]) - ord('a')] = 2
+  
+    for i in range(0, MAX_CHAR): 
+        if(present[i] == 1 or present[i] == 2): 
+            print(chr(i + ord('a')), end = " ") 
+            
+            
+str1 = str(input())
+str2 = str(input())
+findAndPrintUncommonChars(str1, str2) 
+  
