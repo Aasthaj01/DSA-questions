a4c37f2e8bb6b9a48f84a2e833eec6e94a69ed12
Aastha <62432620+Aasthaj01@users.noreply.github.com>
2020-12-11 09:01:52 +0530

    Changed the code

--- searching_sorting/merge_sort.py
+++ searching_sorting/merge_sort.py
@@ -1,53 +1,48 @@
 # Merge sort i.e. divide and conquer algorithm
 # efficient in solving big data sorting problems in nlogn time
 
-def merge_sort(arr, first,  middle, last):
-    n1 = middle - first + 1
-    n2 = last- middle 
+def mergeSort(arr):
+    if len(arr) > 1:
  
-    L = [0] * (n1) 
-    R = [0] * (n2) 
-   
-    for i in range(0 , n1): 
-        L[i] = arr[first + i] 
-  
-    for j in range(0 , n2): 
-        R[j] = arr[middle + 1 + j] 
-
-    i = j =0     # Initial index of first subarray  
-    k = first     # Initial index of merged subarray 
-  
-    while i < n1 and j < n2 : 
-        if L[i] <= R[j]: 
-            arr[k] = L[i] 
+        # Finding the mid of the array
+        mid = len(arr)//2
+ 
+        # Dividing the array elements
+        L = arr[:mid]
+ 
+        # into 2 halves
+        R = arr[mid:]
+ 
+        # Sorting the first half
+        mergeSort(L)
+ 
+        # Sorting the second half
+        mergeSort(R)
+ 
+        i = j = k = 0
+ 
+        # Copy data to temp arrays L[] and R[]
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+ 
+        # Checking if any element was left
+        while i < len(L):
+            arr[k] = L[i]
             i += 1
-        else: 
-            arr[k] = R[j] 
-            j += 1
-        k += 1
-  
-    
-    while i < n1: 
-        arr[k] = L[i] 
-        i += 1
-        k += 1
+            k += 1
  
-    while j < n2: 
-        arr[k] = R[j] 
-        j += 1
-        k += 1
-    
-def merge_sort_2(arr, first, last):
-    if first<last:
-        middle = (first+(first+last)) // 2
-        merge_sort_2(arr, first, middle)
-        merge_sort_2(arr, middle+1, last)
-        merge_sort(arr, first, middle, last)
-        
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+    return arr 
 
 arr = list(map(int, input().split()))
 n = len(arr)
-merge_sort_2(arr, 0, n-1)
-print ("Sorted array is:") 
-for i in range(n): 
-    print (arr[i], end = " ")
+print(mergeSort(arr))
